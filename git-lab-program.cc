43bf672c9d15a4fd86cb98ea5c200ca90deed1bb
draden-mccullough <dm310223@ohio.edu>
2024-05-18 12:04:29 -0400

    Update git-lab-program.cc

--- git-lab-program.cc
+++ git-lab-program.cc
@@ -1,1 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Draden McCullough
+ *        Date: 05/17/2024
+ * Description: Answer 14 HW1
+ */
 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
